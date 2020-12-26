042c911862d3fc2490e7bdecc8f503b84a619920
justweihong <justweihong@gmail.com>
2020-12-26 14:57:27 +0800

    Modify contract structure to eliminate hard coded 50 limit

--- smart-contracts/contracts/CharityChain.sol
+++ smart-contracts/contracts/CharityChain.sol
@@ -2,14 +2,21 @@ pragma solidity >=0.5.16 <0.8.0;
 
 
 contract CharityChain {
-    address admin;  // adminstrator of this charity
-    string public uen; // UEN of charity organisation÷
+    address admin;  // Charity's admin
+    string public uen; // Charity's UEN
     uint constant MAX_LENGTH = 50; // max number of transactions returned for a search by donor
 
+    struct Donation {
+      uint amount; // Amount in cents
+      uint date; // DDMMYYYY format
+      string message; // Any message
+    }
 
-    bytes32[] nricHashes;
-    uint[] amounts;
-    bytes32[] messages;
+    // Maps each donor's nricHashes with the total donation counts 
+    mapping(bytes32 => uint256) donationCounts;
+
+    // Maps each donor's nricHashes with the table of donations
+    mapping(bytes32 => mapping(uint256 => Donation)) donations;
 
     constructor(address _admin, string memory _uen) public {
         admin = _admin;
@@ -20,31 +27,21 @@ contract CharityChain {
         require(msg.sender == admin, 'Only admin can access this function');
         _;
     }
-    // modifier isCharity {
-    //   require(msg.sender in)
-    // }
 
-    function addTransactions(bytes32 nricHash, uint amount, bytes32 message) public isAdmin {
-        nricHashes.push(nricHash);
-        amounts.push(amount);
-        messages.push(message);
+    function addDonation(bytes32 nricHash, uint amount, uint date, string memory message) public isAdmin {
+      Donation memory newDonation = Donation(amount, date, message);
+      uint size = donationCounts[nricHash];
+      donations[nricHash][size] = newDonation;
+      donationCounts[nricHash] = size + 1;
     }
 
-    function checkTransactions(bytes32 nricHash) public view returns (
-        uint[MAX_LENGTH] memory filteredAmounts, bytes32[MAX_LENGTH] memory filteredMessages) {
-        uint index = 0; // index only for filtered arrays.
-        for (uint i = 0; i < nricHashes.length && i <= MAX_LENGTH; i++) {
-            if (nricHashes[i] == nricHash) {
-                filteredAmounts[index] = amounts[i];
-                filteredMessages[index] = messages[i];
-                index++;
-            }
-        }
-        return (filteredAmounts, filteredMessages);
+    function getDonationCount(bytes32 nricHash) public view returns (uint) {
+      return donationCounts[nricHash];
     }
-    
-    function showAllTransactions() public view returns (bytes32[] memory, uint[] memory, bytes32[] memory) {
-        return (nricHashes, amounts, messages);
+
+    function getDonation(bytes32 nricHash, uint index) public view returns (uint, uint, string memory) {
+      Donation memory donation = donations[nricHash][index];
+      return (donation.amount, donation.date, donation.message);
     }
 }
 
