86d6ac18a9f86179b12e703d6887e7ea92370ccd
bharathcs <bharath.sudheer@gmail.com>
2021-01-05 12:10:41 +0800

    Add UserRecordsTable

--- react-app/src/views/Components/DonationsTable.js
+++ react-app/src/views/Components/DonationsTable.js
@@ -89,3 +89,44 @@ export class AllDonationsTable extends React.Component {
     );
   }
 }
+
+export class UserRecordTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nricHash: props.nricHash,
+      donations: [],
+    };
+  }
+
+  async componentDidMount() {
+    if (this.state.donations.length === 0) {
+      console.log("awaiting");
+      const result = await contractFunctions.getAllUserDonations(
+        this.state.nricHash,
+        web3
+      );
+      await setTimeout(() => {
+        this.setState({ donations: result });
+        //TODO: Have this work without the 2000 ms
+        //TODO: Sort data & process
+      }, 2000);
+    }
+  }
+
+  render() {
+    const columnHeader = [
+      // Amount Date Donor Message
+      { id: "donor", label: "Donor", minWidth: 170, align: "left" },
+      { id: "amount", label: "Amount", minWidth: 170, align: "right" },
+      { id: "date", label: "Date", minWidth: 170, align: "left" },
+      { id: "message", label: "Message", minWidth: 170, align: "left" },
+    ];
+
+    return (
+      <div>
+        <Table rows={this.state.donations} columns={columnHeader}></Table>
+      </div>
+    );
+  }
+}
