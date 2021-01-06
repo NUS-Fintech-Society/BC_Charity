961f9593e213616f23913a10242b59186914af0e
bharathcs <bharath.sudheer@gmail.com>
2021-01-06 23:45:40 +0800

    Adjust how data is rendered in the donation tables

--- react-app/src/views/Components/DonationsTable.js
+++ react-app/src/views/Components/DonationsTable.js
@@ -5,6 +5,33 @@ import Table from "./Table";
 const contractFunctions = require("../../contracts/utils/functions");
 const web3 = contractFunctions.getWeb3();
 
+function processDonationRecords(records) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  records.sort((a, b) => Number(a.date) - Number(b.date));
+  records.forEach((value) => {
+    let day = value.date.slice(0, 2);
+    let mth = value.date.slice(2, 4);
+    let yr = value.date.slice(4);
+    value.strDate = day + " " + months[Number(mth) - 1] + " " + yr;
+    let amt = value.amount;
+    value.strAmount = "$" + (Number(amt) / 100).toFixed(2);
+  });
+  return records;
+}
+
 export class OrgRecordTable extends React.Component {
   constructor(props) {
     super(props);
@@ -22,9 +49,9 @@ export class OrgRecordTable extends React.Component {
         web3
       );
       await setTimeout(() => {
-        this.setState({ donations: result });
+        const processedDonations = processDonationRecords(result);
+        this.setState({ donations: processedDonations });
         //TODO: Have this work without the 2000 ms
-        //TODO: Sort data & process
       }, 2000);
     }
   }
@@ -33,8 +60,8 @@ export class OrgRecordTable extends React.Component {
     const columnHeader = [
       // Amount Date Donor Message
       { id: "donor", label: "Donor", minWidth: 170, align: "left" },
-      { id: "amount", label: "Amount", minWidth: 170, align: "right" },
-      { id: "date", label: "Date", minWidth: 170, align: "left" },
+      { id: "strAmount", label: "Amount", minWidth: 170, align: "right" },
+      { id: "strDate", label: "Date", minWidth: 170, align: "left" },
       { id: "message", label: "Message", minWidth: 170, align: "left" },
     ];
 
@@ -59,15 +86,15 @@ export class AllDonationsTable extends React.Component {
       console.log("awaiting");
       const result = await contractFunctions.getAllDonations(web3);
       await setTimeout(() => {
-        const processed = result.map((value) => {
+        let processed = result.map((value) => {
           const { charity, amount, date, message, donor } = value;
           const charityName = charity.name;
           return { charityName, amount, date, message, donor };
         });
+        processed = processDonationRecords(processed);
         this.setState({ donations: processed });
         console.log(this.state.donations);
         //TODO: Have this work without the 2000 ms
-        //TODO: Sort data & process
       }, 2000);
     }
   }
@@ -76,9 +103,9 @@ export class AllDonationsTable extends React.Component {
     const columnHeader = [
       // Amount Date Donor Message
       { id: "charityName", label: "Charity", minWidth: 170, align: "left" },
-      // { id: 'donor', label: 'Donor', minWidth: 170, align: 'left', },
-      { id: "amount", label: "Amount", minWidth: 170, align: "right" },
-      { id: "date", label: "Date", minWidth: 170, align: "left" },
+      // { id: "donor", label: "Donor", minWidth: 170, align: "left" },
+      { id: "strAmount", label: "Amount", minWidth: 170, align: "right" },
+      { id: "strDate", label: "Date", minWidth: 170, align: "left" },
       { id: "message", label: "Message", minWidth: 170, align: "left" },
     ];
 
@@ -107,9 +134,9 @@ export class UserRecordTable extends React.Component {
         web3
       );
       await setTimeout(() => {
-        this.setState({ donations: result });
+        const processed = processDonationRecords(result);
+        this.setState({ donations: processed });
         //TODO: Have this work without the 2000 ms
-        //TODO: Sort data & process
       }, 2000);
     }
   }
@@ -118,8 +145,8 @@ export class UserRecordTable extends React.Component {
     const columnHeader = [
       // Amount Date Donor Message
       { id: "donor", label: "Donor", minWidth: 170, align: "left" },
-      { id: "amount", label: "Amount", minWidth: 170, align: "right" },
-      { id: "date", label: "Date", minWidth: 170, align: "left" },
+      { id: "strAmount", label: "Amount", minWidth: 170, align: "right" },
+      { id: "strDate", label: "Date", minWidth: 170, align: "left" },
       { id: "message", label: "Message", minWidth: 170, align: "left" },
     ];
 
