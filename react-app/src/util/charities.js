ef9fc11ee596ada2c0b038ba123caa6b46f46589
bentanjunrong <seraphi123@yahoo.com.sg>
2021-01-08 00:25:37 +0800

    Updated util/charities.js with firebase info

--- react-app/src/util/charities.js
+++ react-app/src/util/charities.js
@@ -19,15 +19,139 @@ export const columns = [
   },
 ];
 
+// export const charities = [
+//   { name: 'Character & Strength Academy Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '199252597N', contract: "0xC897d46a255004c3E2b46FbD5Ac726d866Ebf5d7" },
+//   { name: 'Jason & Jaffrey Childcare Centre Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '255921113D', contract: "0xEeD494fdCD9287c4B223Fa8810A83E822Da0A150" },
+//   { name: 'Bobby Foundation Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '02996473B', contract: "0xde3653964686daAC5a96A14bfD096706022aE5cA" },
+//   { name: 'Scent Co. Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '251323312W', contract: "0xc7474bc22eA586A01eac34951b3207d6ECf70D8c" },
+//   { name: 'Power Beyond Expectations Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '199556151G', contract: "0x6c262aa3a7333933BC33f9F7019D7A59021F9dC1" },
+//   { name: 'The Life Church', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '37090055C', contract: "0xE34a7f5fC9d653Fb510494E857A387aA1426a4E4" },
+//   { name: 'Nebby Cares Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '255958748C', contract: "0xda532bb1cdf942cB2802EEb70BdbB9375b9203D5" },
+//   { name: 'The Bred Education Fund', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '02986926L', contract: "0x1Ed8BD5b9FfeC35A8F1a5fad3993bb6B8Fc3180B" },
+//   { name: 'The Strength of Gods love', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '03312135B', contract: "0xB8746a8fad46aDbEA4FA188956ef38FDF6350960" },
+//   { name: 'Mobile for Aids', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '259232333H', contract: "0xe853F05E05D0ab97CD46BCAcf422384781f0Ed61" }
+// ]
+
 export const charities = [
-  { name: 'Character & Strength Academy Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '199252597N', contract: "0xC897d46a255004c3E2b46FbD5Ac726d866Ebf5d7" },
-  { name: 'Jason & Jaffrey Childcare Centre Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '255921113D', contract: "0xEeD494fdCD9287c4B223Fa8810A83E822Da0A150" },
-  { name: 'Bobby Foundation Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '02996473B', contract: "0xde3653964686daAC5a96A14bfD096706022aE5cA" },
-  { name: 'Scent Co. Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '251323312W', contract: "0xc7474bc22eA586A01eac34951b3207d6ECf70D8c" },
-  { name: 'Power Beyond Expectations Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '199556151G', contract: "0x6c262aa3a7333933BC33f9F7019D7A59021F9dC1" },
-  { name: 'The Life Church', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '37090055C', contract: "0xE34a7f5fC9d653Fb510494E857A387aA1426a4E4" },
-  { name: 'Nebby Cares Limited', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '255958748C', contract: "0xda532bb1cdf942cB2802EEb70BdbB9375b9203D5" },
-  { name: 'The Bred Education Fund', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '02986926L', contract: "0x1Ed8BD5b9FfeC35A8F1a5fad3993bb6B8Fc3180B" },
-  { name: 'The Strength of Gods love', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '03312135B', contract: "0xB8746a8fad46aDbEA4FA188956ef38FDF6350960" },
-  { name: 'Mobile for Aids', admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14', UEN: '259232333H', contract: "0xe853F05E05D0ab97CD46BCAcf422384781f0Ed61" }
+  {
+    "website": "www.characterandstrength.com",
+    "cause": "education",
+    "contract": "0x042C397bBd62E2f7f9F402e58Ee967D0DE1d2EcA",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.designcrowd.com%2Fdesign%2F7840631&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAD",
+    "name": "Character & Strength Academy Limited",
+    "description": "helping children learn",
+    "UEN": "199252597N",
+    "address": "8 Sinaran Drive #03-01 Oasia Hotel Novena, Singapore 307470",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0xC897d46a255004c3E2b46FbD5Ac726d866Ebf5d7"
+  },
+  {
+    "address": "38 South Buona Vista Rd, Singapore 118164",
+    "contract": "0x2a73825988101317F9117323A72568F79aD99f13",
+    "website": "www.jasonandjaffrey.com",
+    "description": "befriend the elderly",
+    "cause": "elderly",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.creativebloq.com%2Fnews%2Fyoutube-launches-a-new-logo-design&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAF",
+    "UEN": "255921113D",
+    "name": "Jason & Jaffrey Childcare Centre Limited",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0xEeD494fdCD9287c4B223Fa8810A83E822Da0A150"
+  },
+  {
+    "cause": "youth",
+    "name": "Bobby Foundation Limited",
+    "description": "helping youth at risk",
+    "address": "5 Simon Rd, Singapore 545893",
+    "website": "www.bobbyfoundation.com",
+    "UEN": "02996473B",
+    "contract": "0x3405F2d4519f3b3C54b2686eED0fDc7CB7bcAb33",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-vector%2Fabstract-logo-with-colorful-leaves_810102.htm&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAN",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0xde3653964686daAC5a96A14bfD096706022aE5cA"
+
+  },
+  {
+    "name": "Scent Co. Limited",
+    "website": "www.scentco.com",
+    "UEN": "251323312W",
+    "contract": "0x52F2C2FE713a3a8d734d6Dc9D4057184e448BbC0",
+    "cause": "environment",
+    "address": "28 Lor Ampas, Singapore 328781",
+    "description": "encourage the public to recycle",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Flogomaster.ai%2F&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAP",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0xc7474bc22eA586A01eac34951b3207d6ECf70D8c"
+  },
+  {
+    "UEN": "199556151G",
+    "cause": "art appreciation",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nasa.gov%2Faudience%2Fforstudents%2F5-8%2Ffeatures%2Fsymbols-of-nasa.html&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAR",
+    "name": "Power Beyond Expectations Limited",
+    "address": "64 Prinsep St, Singapore 188667",
+    "website": "www.powerbeyond.com",
+    "contract": "0x9AAB1c8c23C28264a247E6c08a34116fB60756be",
+    "description": "Culture art conservation or creation",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0x6c262aa3a7333933BC33f9F7019D7A59021F9dC1"
+  },
+  {
+    "cause": "education",
+    "address": "37 Kampong Bahru Rd, Singapore 169356",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Flogo.html&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAY",
+    "website": "www.thelifechurch.com",
+    "name": "The Life Church",
+    "UEN": "37090055C",
+    "description": "children education and activities",
+    "contract": "0xb7D92425A2Eb78aCFb46089F5E381Cda4A4578d6",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0xE34a7f5fC9d653Fb510494E857A387aA1426a4E4"
+  },
+  {
+    "cause": "animal rights",
+    "website": "www.nebbycares.com",
+    "name": "Nebby Cares Limited",
+    "contract": "0x81390b0C64e4Bc6FEA17EA2725Bce2a57D5f64A7",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Flogo&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAb",
+    "description": "animal protection and welfare",
+    "UEN": "255958748C",
+    "address": "3 Temasek Blvd, 02-300/301, Singapore 038983",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0xda532bb1cdf942cB2802EEb70BdbB9375b9203D5"
+  },
+  {
+    "cause": "education",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.theverge.com%2F2017%2F12%2F19%2F16795334%2Fkon-apple-lawsuit-app-store-logo-sex-pistols&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAj",
+    "UEN": "02986926L",
+    "contract": "0xDcD675a35E3E47c2d8B4aF7eE1C8e63F76bc3964",
+    "website": "www.thebrededucationfund.com",
+    "address": "101 E Coast Rd, Singapore 428796",
+    "description": "children education and activities",
+    "name": "The Bred Education Fund",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0x1Ed8BD5b9FfeC35A8F1a5fad3993bb6B8Fc3180B"
+  },
+  {
+    "UEN": "03312135B",
+    "address": "17D Lor Liput, Singapore 277731",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Flogo%2F&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAq",
+    "contract": "0xAFD7e63d19cCc20947c1FA91501Ca16D498D10CF",
+    "name": "The Strength of Gods love",
+    "description": "Environmental, disaster or long-term based",
+    "website": "www.strengthofgodslove.com",
+    "cause": "environment",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0xB8746a8fad46aDbEA4FA188956ef38FDF6350960"
+  },
+  {
+    "contract": "0x8A9b0d4179C9Ce2f768252f57e7C8c692553B058",
+    "address": "753 North Bridge Rd, Singapore 198721",
+    "website": "www.mobileforaids.com",
+    "cause": "art",
+    "logo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.logodesign.net%2F&psig=AOvVaw0kUxEok-8I7EVAH_kqvy5d&ust=1609493285018000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIju-u_z9-0CFQAAAAAdAAAAABAu",
+    "name": "Mobile for Aids",
+    "description": "Culture art conservation or creation",
+    "UEN": "259232333H",
+    admin: '0xF87d7aee9C262249C5ebb1424a2FDE86A68D1c14',
+    contract: "0xe853F05E05D0ab97CD46BCAcf422384781f0Ed61"
+  }
 ]
