b9cc02fe429bae52403354ebaa0231e1ee106a0c
bentanjunrong <seraphi123@yahoo.com.sg>
2021-01-13 23:57:18 +0800

    Removed unused component route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,7 +4,6 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 
 // pages for this product
-import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 import VerifyPage from "views/VerifyPage/VerifyPage.js";
@@ -21,7 +20,6 @@ function App() {
           <Route path='/' exact component={LandingPage} />
           <Route path='/org/:uen/addtxn' component={AddTxnPage} />
           <Route path='/org/:uen' component={ProfilePage} />
-          <Route path='/component' component={Components} />
           <Route path='/verify' component={VerifyPage} />
           <Route path='/invalid-uen' component={InvalidPage} />
         </Switch>
