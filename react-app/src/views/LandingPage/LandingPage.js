7cb8300931c06482c435b02dda136f425f0dc994
garysyndromes <garylimky@hotmail.com>
2021-01-19 00:06:28 +0800

    landing page minor fixes

--- react-app/src/views/LandingPage/LandingPage.js
+++ react-app/src/views/LandingPage/LandingPage.js
@@ -1,11 +1,7 @@
 import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
-// @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-
-// @material-ui/icons
-
 // core components
 import Header from "components/Header/Header.js";
 import GridContainer from "components/Grid/GridContainer.js";
